f09e13b4d6db65fc7232a996e6863f666d4da124
John Farina <john.farina0824@gmail.com>
2019-02-19 23:17:02 -0500

    A simple script to count the vowels in a given input

---
+++ count_vowels.py
@@ -0,0 +1,17 @@
+"""
+A simple script to count the number of vowels in a sentence given as input.
+"""
+
+
+def count_vowels():
+    vowels = 0
+    count_vowel = input("Please enter a word or phrase: ").lower()
+    vowel_dic = ['a', 'e', 'i', 'o', 'u']
+    for vowel in count_vowel:
+        if vowel in vowel_dic:
+            vowels += 1
+
+    print("Your phrase has " + str(vowels) + " vowels.")
+
+
+count_vowels()
