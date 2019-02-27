7c01dfa7a743a6157861d5c9f4c333fa4392e1a9
John Michael Farina <john.farina0824@gmail.com>
2019-02-27 04:36:28 -0500

    Update Summation Calculator.py

--- Summation Calculator.py
+++ Summation Calculator.py
@@ -1,18 +1,7 @@
 """
-Author: John Farina
-Purpose: The purpose of this program is to calculate the sum of the numbers
-        between a set of two numbers.
+The purpose of this program is to calculate the sum of the numbers
+between a set of two numbers.
 """
-
-
-# Summation
-
-print("Author: John Farina \n")
-
-# Title of program, including parameters that should be followed in order for the program to run correctly
-print("Summation Calculator")
-print("**Inputs must be positive integers, characters and/or negative integers will not be accepted**")
-print("**The first integer must be less than or equal to the second integer**\n")
 
 # While statement that tests to see if user input follows the set parameters
 # If the user input is incorrect, the program will notify the user to try their input again.
@@ -40,4 +29,4 @@ def result():
 
 # Print statement that returns the user's inputs as well as the summation of those inputs
 print("The result of the summation between the integers {} and {} is: {}\n"
-      .format(firstNum, secondNum, result()))
+.format(firstNum, secondNum, result()))
