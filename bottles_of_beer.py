f1f86855b06dfd2463996f6e1e7c6cd6a79f15f5
John Farina <john.farina0824@gmail.com>
2019-03-04 02:53:14 -0500

    A simple program that recites the lyrics of "99 Bottles of Beer".

---
+++ bottles_of_beer.py
@@ -0,0 +1,28 @@
+"""
+This program follows the song lyrics of '99 Bottles of Beer'.
+In it, the program goes through a loop that will subtract one from
+the starting bottles number of 99 until it reaches zero.
+"""
+
+
+def count_bottles():
+    bottles = 99
+    while True:
+        if bottles >= 2:
+            print(str(bottles) + " bottles of beer on the wall, " 
+            + str(bottles) + " bottles of beer.")
+            bottles -= 1
+            print("You take one down and pass it around, " 
+            + str(bottles) + " bottles of beer of the wall.\n")
+        elif bottles == 1:
+            print(str(bottles) + " bottle of beer on the wall " 
+            + str(bottles) + " bottle of beer.")
+            bottles = 0
+            print("You take one down and pass it around, " 
+            + str(bottles) + " bottles of beer of the wall.")
+        else:
+            break
+
+
+if __name__ == '__main__':
+    count_bottles()
