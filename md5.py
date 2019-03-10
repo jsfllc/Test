8e9b9d920313453280c8882e40b4a198f5adb2e7
John Farina <john.farina0824@gmail.com>
2019-03-09 20:00:00 -0600

    MD5 Converter program
    This program will take the inputted string of the user and then output the respective md5 hash value.

---
+++ md5.py
@@ -0,0 +1,16 @@
+"""
+This program asks a user for a word or phrase, then takes that word or phrase and outputs
+its respected MD5 hash value.
+"""
+
+import hashlib
+
+def md5_converter():
+    phrase = input("What is the word or phrase you would like the MD5 hash of?: ")
+    hashed = hashlib.md5(phrase.encode())
+
+    print("\nThe md5 hash of your phrase is: ", end ="")
+    print(hashed.hexdigest())
+
+if __name__ == '__main__':
+    md5_converter()
