0bf1a5c793f1a63436eed3a90b9ecf5e7a9296a8
John Michael Farina <john.farina0824@gmail.com>
2019-03-16 22:51:53 -0400

    Update merge_names.py

--- merge_names.py
+++ merge_names.py
@@ -3,22 +3,27 @@ The purpose of this program is to combine a list of first names
 with a list of last names, and, in turn, form a first and last name list
 """
 
-# Input that asks users for a list of first names separated by a comma
-# and then sets those inputs into values in the firstList list
-firstName = input("Please provide list of first names separated by comma, e.g. John,Kim,Kate: ")
-firstList = list(map(str, firstName.split(',')))
 
-# Input that asks users for a list of last names separated by a comma
-# and then sets those inputs into values in the lastList list
-lastName = input("Please provide list of last names separated by comma, e.g. Davidson,Hunter,Johnson: ")
-lastList = list(map(str, lastName.split(',')))
+def merge():
+    # Input that asks users for a list of first names separated by a comma
+    # and then sets those inputs into values in the firstList list
+    firstName = input("Please provide list of first names separated by comma, e.g. John,Kim,Kate: ")
+    firstList = list(map(str, firstName.split(',')))
 
-# Initializing the firstLastList list, which combines both the firstList and lastList, above
-firstLastList = list(zip(firstList, lastList))
-print()
+    # Input that asks users for a list of last names separated by a comma
+    # and then sets those inputs into values in the lastList list
+    lastName = input("Please provide list of last names separated by comma, e.g. Davidson,Hunter,Johnson: ")
+    lastList = list(map(str, lastName.split(',')))
 
-# Output of the firstList and lastList lists and then zips the entities together into
-# (firstname and lastname) pairs
-print("The list of first names is: " + str(firstList))
-print("The list of last names is: " + str(lastList))
-print("The list of first and last names is: " + str(firstLastList).replace(',', ''))
+    # Initializing the firstLastList list, which combines both the firstList and lastList, above
+    firstLastList = list(zip(firstList, lastList))
+
+    # Output of the firstList and lastList lists and then zips the entities together into
+    # (firstname and lastname) pairs
+    print("The list of first names is: " + str(firstList))
+    print("The list of last names is: " + str(lastList))
+    print("The list of first and last names is: " + str(firstLastList).replace(',', ''))
+
+if __name__ == '__main__':
+    merge()
+    
