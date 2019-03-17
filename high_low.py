80c55ae548a76e74bf253a632800966388fd2717
John Michael Farina <john.farina0824@gmail.com>
2019-03-16 23:36:16 -0400

    Update high_low.py

--- high_low.py
+++ high_low.py
@@ -5,44 +5,62 @@ will guess a user's number that is in between 1 and a higher
 number that is decided by the user in seven guesses.
 """
 
-print("I am willing to bet I can guess any number in the range of your "
-      "choosing.\n")
+import sys
 
-guessList = 0   # Variable that keeps count of number of guesses
-lowNumber = 1   # Variable that represents the beginning of the range
 
-# While statement to make sure user is inserting a positive integer
-while True:
-    try:
-        highNumber = int(input("The top number of your range, starting at one, is: "))  # User input to determine the end of the range
-    except ValueError:
-        print("Invalid response, please enter an integer.")
-        continue
-      
-    if highNumber < 0:
-        print("Please insert a positive integer.")
-    else:
-        break
+class HighLow:
+    def __init__(self):
+        self.test()
 
-# While statement that takes user input and determines the best course of action
-while True:
-    guess = (lowNumber + highNumber) // 2   # Equation that makes it possible for computer to guess the user's number
-    print("Your number has to be {}, right?".format(guess))
-    hiLowInput = input("Let me know if the guess is too high with >, "
-                       "to low with <, "
-                       "or y if I guessed your number. ".lower())
-    guessList += 1  # Adds 1 to the guess count every time the computer guesses the user's number
+    def test(self):
+        print("I am willing to bet I can guess any number in the range of your "
+              "choosing.\n")
+        # While statement to make sure user is inserting a positive integer
+        while True:
+            try:
+                high_number = int(input("The top number of your range, starting at one, is: "))  # User input to
+                # determine the end of the range
+            except ValueError:
+                print("Invalid response, please enter an integer.\n")
+                continue
 
-    # if, elif, and else statements that determines what output is appropriate depending on the user's input
-    if hiLowInput == ">":
-        highNumber = guess
-        guess = (lowNumber + guess) // 2
-    elif hiLowInput == "<":
-        lowNumber = guess
-        guess = (highNumber + lowNumber) // 2
-    elif hiLowInput == "y":
-        guessing = False
-        print("\nI guessed your number in {} tries! Your number was {}.".format(guessList, guess))
-    else:
-        print("\nIncorrect input, please enter >, <, or y.\n")
-print("Thanks for playing!")
+            if high_number < 0:
+                print("Please insert a positive integer.\n")
+
+            else:
+                self.high_low_guess(high_number)
+
+    def high_low_guess(self, high_number):
+        guess_list = 0   # Variable that keeps count of number of guesses
+        low_number = 1   # Variable that represents the beginning of the range
+
+        # While statement that takes user input and determines the best course of action
+        while True:
+            # Equation that makes it possible for computer to guess the user's number
+            guess = (low_number + high_number) // 2
+            print("\nYour number has to be {}, right?".format(guess))
+            hi_low_input = input("Let me know if the guess is too high with >, to low with <, "
+                                 "or y if I guessed your number. ".lower())
+
+            # if, elif, and else statements that determines what output is appropriate depending on the user's input
+            if hi_low_input == ">":
+                high_number = guess
+                (low_number + guess) // 2
+                guess_list += 1
+
+            elif hi_low_input == "<":
+                low_number = guess
+                (high_number + low_number) // 2
+                guess_list += 1
+
+            elif hi_low_input == "y":
+                print("\nI guessed your number in {} tries! Your number was {}.".format(guess_list, guess))
+                print("Thanks for playing!")
+                sys.exit()
+
+            else:
+                print("\nIncorrect input, please enter >, <, or y.")
+
+
+if __name__ == '__main__':
+    HighLow()
